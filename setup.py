8d874102011a5fcc08487d2d55389e5560ca2740
Saley-Sujan <Saleysujan96@gmail.com>
2024-11-10 18:24:22 +0100

    git add . git commit -m "Add setup.py to make the repository pip-installable" git commit -m "Add requirements.txt for managing dependencies" git commit -m "Add _init_.py to define the package structure"
    git add .
    git commit -m "Add setup.py to make the repository pip-installable"
    git commit -m "Add requirements.txt for managing dependencies"
    git commit -m "Add _init_.py to define the package structure"

--- setup.py
+++ setup.py
@@ -1,25 +1,20 @@
-# setup.py
-
 from setuptools import setup, find_packages
 
 setup(
-    name='math-quiz',
-    version='0.1',
+    name="math-quiz",
+    version="1.0",
     packages=find_packages(),
-    include_package_data=True,
-    license='MIT',
-    description='A simple math quiz application.',
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    author='Saley-Sujan',
-    author_email='saleysujan96@gmail.com',
-    url='https://github.com/Saley-Sujan/dsss_homework_2.git',
+    install_requires=[],  # Add dependencies here if needed
+    author="Saley-Sujan",
+    author_email="saleysujan96@gmail.com",
+    description="A simple math quiz game with random questions.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Saley-Sujan/dsss_homework_2",
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    scripts=['scripts/math_quiz'],
-    install_requires=[
-        # List dependencies here if needed
-    ],
+    python_requires=">=3.6",
 )
